8fd4337cb63a63f7aaf108cfeba90c7043d02e83
Macbook Air <air@MacbookdeMacBook-Air.local>
2016-10-13 15:27:44 +0800

    完成中间件

--- forum/settings.py
+++ forum/settings.py
@@ -58,7 +58,20 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middlewares.PrintParamsMiddleware',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'middlewares.PrintParamsMiddleware',
+    'middlewares.logExceptionMiddleware',
+
 ]
 
 ROOT_URLCONF = 'forum.urls'
